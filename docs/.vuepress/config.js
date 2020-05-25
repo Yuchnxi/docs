b1882290b2c66a55ea1774e1a637e056ebe6f2c4
yuchenxi6 <yuchenxi748521@163.com>
2020-05-25 22:06:37 +0800

    提交评论功能

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -1,7 +1,6 @@
 const moment = require('moment');
 
 module.exports = {
-  base: "/docs/",
   title: "yuchenxi",
   description: "yuchenxi",
   head: [
@@ -9,17 +8,34 @@ module.exports = {
     ['meta', { name: 'author', content: 'yuchenxi' }],
     ['meta', { name: 'keywords', content: 'vuepress 介绍, vuepress 说明, yuchenxi' }]
   ],
-  plugins: [
-    [
-      '@vuepress/last-updated',
+  plugins: {
+      '@vuepress/last-updated':
       {
         transformer: (timestamp) => {
           moment.locale("zh-cn")
           return moment(timestamp).format('LLLL')
         }
+      },
+      '@vuepress/pwa':
+      {
+        serviceWorker: true,
+        updatePopup: {
+          message: "发现新的内容可用",
+          buttonText: "刷新"
+        }
+      },
+      '@vssue/vuepress-plugin-vssue':
+       {
+          // 设置 `platform` 而不是 `api`
+          platform: 'github-v4',
+  
+          // 其他的 Vssue 配置
+          owner: 'Yuchnxi',
+          repo: 'docs',
+          clientId: '067350aff1f6e659f70f',
+          clientSecret: 'ef6712ddf106693c55e2c501036915b0e5db510f',
       }
-    ]
-  ],
+  },
   themeConfig: {
     // 最后更新时间
     lastUpdated: '更新时间', // string | boolean
